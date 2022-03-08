d6e094a818dffbca471544d9618898f432b756fa
ricdip <riccardodpr@gmail.com>
2022-03-08 23:09:17 +0100

    tic tac toe

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 
 int main(void) {
     TicTacToeGame game;
+    int maxDepth = 9;
 
-    game.AIvsAI(9);
-    // game.PlayervsAI(9);
+    // Tic Tac Toe AI vs AI mode
+    game.AIvsAI(maxDepth);
+    // Tic Tac Toe Player vs AI mode
+    // game.PlayervsAI(maxDepth);
 
     return 0;
 }
