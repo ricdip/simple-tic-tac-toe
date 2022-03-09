cb19797fece0ab77268d68af958923121e8cd639
ricdip <riccardodpr@gmail.com>
2022-03-09 01:06:47 +0100

    tic tac toe

---
+++ README.md
@@ -0,0 +1,19 @@
+# C++ Simple Tic Tac Toe
+
+A simple C++ implementation for the Tic Tac Toe game that uses the MinMax Algorithm for the AI implementation
+
+## Implemented algorithms
+
+- MinMax
+
+## Implemented heuristics
+
+- Game over heuristic:
+    - we assign `INT_MAX` score if the max player wins
+    - we assign `INT_MIN` score if the min player wins
+    - we assign `0` score otherwise
+
+## Running the application
+
+`make build`
+`make run`
--- main.cpp
+++ main.cpp
@@ -1,13 +1,61 @@
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+
 #include "game/game.hpp"
 
-int main(void) {
-    TicTacToeGame game;
-    int maxDepth = 9;
+#define AI_VS_AI false
+#define PLAYER_VS_AI true
 
-    // Tic Tac Toe AI vs AI mode
-    game.AIvsAI(maxDepth);
-    // Tic Tac Toe Player vs AI mode
-    // game.PlayervsAI(maxDepth);
+struct Args {
+    int maxDepth;
+    bool mode;
+};
+
+Args parseArgs(int argc, char* argv[]) {
+    if (argc > 1) {
+        int maxDepth = 9;
+        bool mode = false;
+
+        for (int i = 0; i < argc; i++) {
+            if (strcmp(argv[i], "--depth") == 0) {
+                maxDepth = std::atoi(argv[i + 1]);
+            } else if (strcmp(argv[i], "--mode") == 0) {
+                if (strcmp(argv[i + 1], "ai_vs_ai") == 0) {
+                    mode = false;
+
+                } else if (strcmp(argv[i + 1], "player_vs_ai") == 0) {
+                    mode = true;
+
+                } else {
+                    throw "Illegal mode";
+                }
+            }
+        }
+        return Args{maxDepth, mode};
+
+    } else {
+        // default args:
+        //  - depth = 9
+        //  - mode  = AIvsAI
+        return Args{9, false};
+    }
+}
+
+int main(int argc, char* argv[]) {
+    TicTacToeGame game;
+
+    Args parsedArgs = parseArgs(argc, argv);
+
+    if (parsedArgs.mode == AI_VS_AI) {
+        // Tic Tac Toe AI vs AI mode
+        game.AIvsAI(parsedArgs.maxDepth);
+
+    } else if (parsedArgs.mode == PLAYER_VS_AI) {
+        // Tic Tac Toe Player vs AI mode
+        game.PlayervsAI(parsedArgs.maxDepth);
+    }
 
     return 0;
 }
